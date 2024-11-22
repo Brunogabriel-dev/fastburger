af712ad1057ca393ff416d17485842e732a278eb
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-22 20:49:13 -0300

    Inclusão de texto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Download from "./components/Download";
+import Easy from "./components/Easy";
+import Food from "./components/Food";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import {useState} from 'react'
 
 function App() {
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const toggleCartVisibility = () => {
+    setIsCartVisible(prev => !prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar toggleCartVisibility={toggleCartVisibility}/>
+      <Hero/>
+      <Food isCartVisible={isCartVisible}/>
+      <Easy/>
+      <Download/>
+      <Footer/>
     </div>
   );
 }
