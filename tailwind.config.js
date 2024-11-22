2f4b7774e6b6e8bb1b90e3dd94c3e30fab8edbc6
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-22 12:01:10 -0300

    Inclusão de texto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
-      backgroundImage: {'food-image': "url('/src/assets/logoQuikDeli.png')",'food-image-2': "urlToUrlWithoutFlightMarker=('/src/assets/bgfood.png')"}
+      backgroundImage: {'food-image': "url('/src/assets/logoQuikDeli.png')",'food-image-2': "url('/src/assets/bgfood.png')"}
     },
   },
   plugins: [],
